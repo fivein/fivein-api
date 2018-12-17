f6242f745762910b8f13da94aca502cd8a1d7da5
dawidjaja <dalfred2000@gmail.com>
2018-12-18 01:44:03 +0700

    add temporary word timing

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -4,31 +4,36 @@ import { sign } from 'jsonwebtoken'
 import { APP_SECRET, getUserId } from '../utils'
 
 export const Mutation: MutationResolvers.Type = {
-  ...MutationResolvers.defaultResolvers,
-  signup: async (parent, { username, email, password }, context) => {
-    const hashedPassword = await hash(password, 10);
-    const user = await context.prisma.createUser({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    return {
-      token: sign({ userId: user.id }, APP_SECRET),
-      user,
-    }
-  },
-  login: async (parent, { email, password }, context) => {
-    const user = await context.prisma.user({ email });
-    if (!user) {
-      throw new Error(`No user found for email: ${email}`);
-    }
-    const passwordValid = await compare(password, user.password);
-    if (!passwordValid) {
-      throw new Error('Invalid password');
-    }
-    return {
-      token: sign({ userId: user.id }, APP_SECRET),
-      user,
-    }
-  },
+    ...MutationResolvers.defaultResolvers,
+    signup: async (parent, { username, email, password }, context) => {
+        const hashedPassword = await hash(password, 10);
+        const user = await context.prisma.createUser({
+            username,
+            email,
+            password: hashedPassword,
+        });
+        return {
+            token: sign({ userId: user.id }, APP_SECRET),
+            user,
+        }
+    },
+    login: async (parent, { email, password }, context) => {
+        const user = await context.prisma.user({ email });
+        if (!user) {
+            throw new Error(`No user found for email: ${email}`);
+        }
+        const passwordValid = await compare(password, user.password);
+        if (!passwordValid) {
+            throw new Error('Invalid password');
+        }
+        return {
+            token: sign({ userId: user.id }, APP_SECRET),
+            user,
+        }
+    },
+    updateWord: (parent, {word, time}, context) => {
+        const user = getUserId(context);
+        console.log(word + " " + time);
+        return true;
+    },
 };
