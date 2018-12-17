91407e8e727a8fc97319bc9c5f63427902d98065
plty <platyunyu@gmail.com>
2018-12-18 00:47:45 +0700

    set up usernameOrEmail login

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -2,6 +2,7 @@ import { MutationResolvers } from '../generated/graphqlgen'
 import { hash, compare } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import { APP_SECRET, getUserId } from '../utils'
+import { User } from "../generated/prisma-client";
 
 export const Mutation: MutationResolvers.Type = {
   ...MutationResolvers.defaultResolvers,
@@ -17,15 +18,20 @@ export const Mutation: MutationResolvers.Type = {
       user,
     }
   },
-  login: async (parent, { email, password }, context) => {
-    const user = await context.prisma.user({ email });
+  login: async (parent, { usernameOrEmail, password }, ctx) => {
+    const userFromEmail: User = await ctx.prisma.user({ email: usernameOrEmail });
+    const userFromUsername: User = await ctx.prisma.user({ username: usernameOrEmail });
+
+    const user: User = !!userFromEmail ? userFromEmail : userFromUsername;
     if (!user) {
-      throw new Error(`No user found for email: ${email}`);
+      throw new Error(`No user found for username or email: ${usernameOrEmail}`);
     }
+
     const passwordValid = await compare(password, user.password);
     if (!passwordValid) {
       throw new Error('Invalid password');
     }
+
     return {
       token: sign({ userId: user.id }, APP_SECRET),
       user,
