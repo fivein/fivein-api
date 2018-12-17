d0aac2aa21bf150e4ac64698af4492802ee3ec1d
plty <platyunyu@gmail.com>
2018-12-17 20:33:31 +0700

    remove artifact of post from permission middleware

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -6,26 +6,12 @@ const rules = {
     const userId = getUserId(context)
     return Boolean(userId)
   }),
-  isPostOwner: rule()(async (parent, { id }, context) => {
-    const userId = getUserId(context)
-    const author = await context.prisma
-      .post({
-        id,
-      })
-      .author()
-    return userId === author.id
-  }),
-}
+};
 
 export const permissions = shield({
   Query: {
     me: rules.isAuthenticatedUser,
-    filterPosts: rules.isAuthenticatedUser,
-    post: rules.isAuthenticatedUser,
   },
   Mutation: {
-    createDraft: rules.isAuthenticatedUser,
-    deletePost: rules.isPostOwner,
-    publish: rules.isPostOwner,
   },
-})
+});
