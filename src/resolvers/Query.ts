be22bb5ca4f15f48dddcf39b87488ca712e6adff
plty <platyunyu@gmail.com>
2018-12-18 10:22:33 +0700

    refactor Query.me

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,12 +1,10 @@
 import { QueryResolvers } from '../generated/graphqlgen'
-import { getUserId } from '../utils'
+import { getUser } from '../utils'
 
 export const Query: QueryResolvers.Type = {
   ...QueryResolvers.defaultResolvers,
 
-  me: (parent, args, context) => {
-    const userId = getUserId(context);
-    console.log(userId);
-    return context.prisma.user({ id: userId })
+  me: async (parent, args, ctx) => {
+    return await getUser(ctx);
   },
 };
